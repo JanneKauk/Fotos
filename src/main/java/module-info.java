f5eda007e7dfe15a56dcd63456e01021feeec500
kallevo <kalle.voutilainen@metropolia.fi>
2021-09-15 13:46:53 +0300

    -Database tiedosto tehty tietokantaan yhdistämistä varten.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
-module otp1.otpr21fotosdemo {
+module otp.otpr21fotosdemo {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
 
     opens otp1.otpr21fotosdemo to javafx.fxml;
