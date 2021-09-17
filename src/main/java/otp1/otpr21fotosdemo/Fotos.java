274b5a55c0828ecd4c60c05b03c3f9715d9c1635
petriim <petri.immonen2@metropolia.fi>
2021-09-17 13:29:37 +0300

    Lisätty filtterimenulle ja foldermenulle piilotusnapit ja näille animointia. Järjestelty uudelleen layout-paneja ja poistettu turhia. Kuvanlisäysnappi korjattu oikeaan alareunaan.

--- src/main/java/otp1/otpr21fotosdemo/Fotos.java
+++ src/main/java/otp1/otpr21fotosdemo/Fotos.java
@@ -12,7 +12,7 @@ public class Fotos extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Fotos.class.getResource("Fotos.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1280, 800);
-        stage.setTitle("Hello!");
+        stage.setTitle("Fotos!");
         stage.setMaximized(true);
         stage.setScene(scene);
         stage.show();
--- src/main/java/otp1/otpr21fotosdemo/FotosController.java
+++ src/main/java/otp1/otpr21fotosdemo/FotosController.java
@@ -1,13 +1,16 @@
 package otp1.otpr21fotosdemo;
 
-import javafx.event.EventHandler;
+import javafx.animation.RotateTransition;
+import javafx.animation.TranslateTransition;
 import javafx.fxml.FXML;
-import javafx.geometry.Bounds;
 import javafx.scene.control.ContextMenu;
 import javafx.scene.control.Label;
 import javafx.scene.control.MenuItem;
-import javafx.scene.input.MouseEvent;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
 import javafx.scene.shape.Circle;
+import javafx.util.Duration;
 
 public class FotosController {
     @FXML
@@ -44,5 +47,65 @@ public class FotosController {
         double boundsInScenex = profile.localToScene(profile.getBoundsInLocal()).getMaxX();
         double boundsInSceney = profile.localToScene(profile.getBoundsInLocal()).getMaxY();
         menu.show(profile, boundsInScenex, boundsInSceney);
+    }
+
+    @FXML
+    private Pane filterMenu;
+    @FXML
+    private StackPane filterButtonStackPane;
+    @FXML
+    private void onFilterShowHidebuttonClick(){
+        TranslateTransition transitionMenu = new TranslateTransition(new Duration(500), filterMenu);
+        RotateTransition rotateButton = new RotateTransition(new Duration(500), filterButtonStackPane);
+        if (filterMenu.getTranslateX() != 0){
+            //Avataan kiinni oleva filtermenu
+            System.out.println("Filterit auki!");
+            transitionMenu.setToX(0);
+            transitionMenu.play();
+            rotateButton.setByAngle(180);
+            rotateButton.play();
+            filterMenu.setManaged(true);
+        } else {
+            //Suljetaan auki oleva filtermenu
+            System.out.println("Filterit kiinni!");
+            transitionMenu.setToX(-(filterMenu.getWidth()));
+            transitionMenu.play();
+            rotateButton.setByAngle(180);
+            rotateButton.play();
+            transitionMenu.setOnFinished(event -> {
+                filterMenu.setManaged(false);
+            });
+
+        }
+    }
+    @FXML
+    private ScrollPane folderMenu;
+    @FXML
+    private StackPane folderButtonStackPane;
+    @FXML
+    private void onFolderShowHidebuttonClick(){
+        TranslateTransition transitionMenu = new TranslateTransition(new Duration(500), folderMenu);
+        RotateTransition rotateButton = new RotateTransition(new Duration(500), folderButtonStackPane);
+        if (folderMenu.getTranslateY() != 0){
+            //Avataan kiinni oleva filtermenu
+            System.out.println("Folderit auki!");
+            transitionMenu.setToY(0);
+            transitionMenu.play();
+            rotateButton.setByAngle(180);
+            rotateButton.play();
+            folderMenu.setManaged(true);
+        } else {
+            //Suljetaan auki oleva filtermenu
+            System.out.println("Folderit kiinni!");
+            folderMenu.setViewOrder(1);
+            transitionMenu.setToY(-(folderMenu.getHeight()*2));
+            transitionMenu.play();
+            rotateButton.setByAngle(180);
+            rotateButton.play();
+            transitionMenu.setOnFinished(event -> {
+                folderMenu.setManaged(false);
+            });
+
+        }
     }
 }
