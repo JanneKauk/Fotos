e56bbf1c2c53380d2b4fae596819af8cd1815c50
juritihane <juri.tihane@gmail.com>
2021-09-20 10:22:17 +0300

    - Added DB connection handling class

--- src/main/java/otp1/otpr21fotosdemo/database.java
+++ src/main/java/otp1/otpr21fotosdemo/database.java
@@ -4,22 +4,26 @@ import java.sql.*;
 
 public class database {
     public static void main(String[] args) {
-        String url = "jdbc:mysql://10.114.32.13:22/";
-        String db = "testi";
-        String dbuser = "otpdb";
-        String dbpass = "Asdfghjkl1234567890";
-        Connection connection;
-
-        {
-            try {
-                connection = DriverManager.getConnection(url + db, dbuser, dbpass);
-                Statement statement = connection.createStatement();
-                String sql = "INSERT INTO Testi1" +
-                        "VALUES ('gasgadbbcxbrw')";
-                int update = statement.executeUpdate(sql);
-            } catch (SQLException e) {
-                e.printStackTrace();
-                System.out.println("SQL statement is not executed!");
+        System.out.println("\n\n***** MySQL JDBC Connection Testing *****");
+        Connection conn = null;
+        try {
+            String userName = "otpdb";
+            String password = "Asdfghjkl1234567890";
+            String url = "jdbc:mysql://10.114.32.13:3306/";
+            conn = DriverManager.getConnection(url, userName, password);
+            System.out.println("\nDatabase Connection Established...");
+        } catch (Exception ex) {
+            System.err.println("Cannot connect to database server");
+            ex.printStackTrace();
+        } finally {
+            if (conn != null) {
+                try {
+                    System.out.println("\n***** Let terminate the Connection *****");
+                    conn.close();
+                    System.out.println("\nDatabase connection terminated...");
+                } catch (Exception ex) {
+                    System.out.println("Error in connection termination!");
+                }
             }
         }
     }
